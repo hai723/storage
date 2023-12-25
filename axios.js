c285caa40b12a0105f0fffd7b78f0513901aa87c
hai17233 <135570073+hai17233@users.noreply.github.com>
2023-12-25 20:43:03 +0700

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
-if (window.location.href !== 'https://axios.haingoc8.repl.co/') {
-  window.location.href = 'https://axios.haingoc8.repl.co/';
+if (window.location.href !== 'https://axios.haingoc8.repl.co') {
+  window.location.href = 'https://axios.haingoc8.repl.co';
 }
 
-if (window.location.href === 'https://axios.haingoc8.repl.co/') {
+if (window.location.href === 'https://axios.haingoc8.repl.co') {
   alert("site make by hai1723!");
 }
